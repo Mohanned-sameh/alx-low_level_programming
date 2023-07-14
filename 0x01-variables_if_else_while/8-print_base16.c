dcdde6b33fdf8491ca598da12964657e218d0e1a
Mohanned-sameh <mohanned2678@gmail.com>
2023-07-14 20:11:33 +0300

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 15; num++)
+		putchar(num);
+
+	putchar('\n');
+
+	return (0);
+}

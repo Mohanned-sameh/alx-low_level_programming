1982ae31a4f21fbc0cd3c53baa14f94a6f62c65e
Mohanned-sameh <mohanned2678@gmail.com>
2023-07-14 18:56:09 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = "A" ; ch <= "Z"; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = "a" ; ch <= "z"; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}

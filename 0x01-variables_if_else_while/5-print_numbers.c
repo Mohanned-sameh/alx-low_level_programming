67aa5dd98a2f5f91f00b6d7613e1bd84f5df9e0a
Mohanned-sameh <mohanned2678@gmail.com>
2023-07-14 19:50:46 +0300

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,8 +7,8 @@ int main(void)
 {
 	int numbers;
 
-	for (numbers = 0; numbers < 10; numbers++)
-		printf(" %d", numbers);
+	for (numbers = 0; numbers <= 10; numbers++)
+		printf("%d", numbers);
 
 	return (0);
 }

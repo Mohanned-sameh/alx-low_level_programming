2adbbfd76ddd4a9a78e63d22f91bf8fa09987c06
Mohanned-sameh <mohanned2678@gmail.com>
2023-07-19 03:08:03 +0300

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,24 +6,24 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(0);
+		_putchar('0');
 
-		for (j = 0; j < 10; j++)
+		for (j = 1; j <= 9; j++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
 			k = i * j;
 
-			if (k <= 10)
+			if (k <= 9)
 				_putchar(' ');
 			else
 				_putchar((k / 10) + '0');
 
 			_putchar((k % 10) + '0');
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }

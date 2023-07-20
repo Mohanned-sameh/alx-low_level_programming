c87e82f3ccc1bf3b8699d61ec659f6c8f3cbcb4e
Mohanned-sameh <mohanned2678@gmail.com>
2023-07-20 03:02:00 +0300

    fixed comment style

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,7 +7,6 @@ int main(void)
 {
 int i, j;
 
-// Write a program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line?
 for (i = 0; i < 1024; i++)
 {
 if (i % 3 == 0 || i % 5 == 0)

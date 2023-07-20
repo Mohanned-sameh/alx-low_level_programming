91b8fb93eba3a95c184d626dbaf2e89748022e1f
Mohanned-sameh <mohanned2678@gmail.com>
2023-07-20 23:29:58 +0300

    done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - a function that prints 0 - 9 with a new line
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0';c <= '9'; c++)
+	{
+		if (c != 2 && c != 4)
+			_printchar(c);
+	}
+	_printchar('\n');
+}

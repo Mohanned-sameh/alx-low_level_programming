80ba944f05c0b3b669d03462c4a890114ee824bc
Mohanned-sameh <mohanned2678@gmail.com>
2023-07-20 04:00:50 +0300

    fixed

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 /**
-* main - entry point
-* Return: 0
-*/
+ * main - entry point
+ * Return: 0
+ */
 int main(void)
 {
-unsigned long n1 = 0, n2 = 0, nsum;
-float tot_sum;
+    unsigned long n1 = 0, n2 = 0, nsum;
+    float tot_sum;
 
-while (1)
-{
-nsum = n1 + n2;
-if (nsum > 1000000000)
-break;
-if (nsum % 2 == 0)
-tot_sum += nsum;
+    while (1)
+    {
+        nsum = n1 + n2;
+        if (nsum > 1000000000)
+            break;
+        if (nsum % 2 == 0)
+            tot_sum += nsum;
 
-n1 = n2;
-n2 = nsum;
-}
-printf("%.0f\n", tot_sum);
+        n1 = n2;
+        n2 = nsum;
+    }
+    printf('%.0f\n', tot_sum);
 
-return (0);
+    return (0);
 }

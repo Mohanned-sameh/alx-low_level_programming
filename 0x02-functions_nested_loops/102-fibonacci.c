ac243feea1897888857316351cd3702432eae543
Mohanned-sameh <mohanned2678@gmail.com>
2023-07-20 03:39:48 +0300

    removed whitespaces

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 /**
- * main - entry point
- * Return: 0
- */
+* main - entry point
+* Return: 0
+*/
 int main(void)
 {
-    int count;
-    unsigned long fib1, fib2 = 1, sum;
+int count;
+unsigned long fib1, fib2 = 1, sum;
 
-    for (count = 0; count < 50; count++)
-    {
-        sum = fib1 + fib2;
-        printf("%lu", sum);
+for (count = 0; count < 50; count++)
+{
+sum = fib1 + fib2;
+printf("%lu", sum);
 
-        fib1 = fib2;
-        fib2 = sum;
+fib1 = fib2;
+fib2 = sum;
 
-        if (count == 49)
-            printf("\n");
-        else
-            printf(", ");
-    }
-    return (0);
+if (count == 49)
+{
+printf("\n");
+}
+else
+{
+printf(", ");
+}
+}
+return (0);
 }

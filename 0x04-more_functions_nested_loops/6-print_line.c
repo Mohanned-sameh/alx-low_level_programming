31a5a3702d01f69798499c9705b1816078e82b65
Mohanned-sameh <mohanned2678@gmail.com>
2023-07-20 23:51:30 +0300

    done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_line - a function that prints a straight line
+ * @n: the number of times to print _
+ */
+void print_line(int n)
+{
+	if (n > 0)
+		_putchar('_');
+	else
+		_putchar('\n');
+}

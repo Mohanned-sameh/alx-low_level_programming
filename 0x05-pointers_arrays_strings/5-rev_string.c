8152dd3696a28520bd6c0ce853196875372f803d
Mohanned-sameh <mohanned2678@gmail.com>
2023-07-26 07:47:36 +0300

    done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,5 +7,21 @@
  */
 void rev_string(char *s)
 {
-	
+	int len = 0, i = 0;
+
+	while (*(s + len) != '\0')
+	{
+		++len;
+	}
+
+	--len;
+	while (len > i)
+	{
+		char c = *(s + i);
+
+		*(s + i) = *(s + len);
+		*(s + len) = c;
+		--len;
+		++i;
+	}
 }

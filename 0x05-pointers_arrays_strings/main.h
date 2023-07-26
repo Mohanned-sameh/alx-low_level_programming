d40aafcaca36047cb2fdd4a6bf8d4f48b1933e33
Mohanned-sameh <mohanned2678@gmail.com>
2023-07-26 07:39:51 +0300

    done

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,12 @@
-#ifndef
-#define
+#ifndef MAIN.H
+#define MAIN.h
+
+#include <stdio.h>
+
+int _putchar(char c);
 int _strlen(char *s);
 void reset_to_98(int *n);
+void _puts(char *str);
 void swap_int(int *a, int *b);
 #endif
 

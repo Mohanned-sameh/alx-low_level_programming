6c5ce199fbfb4aa784b03d23ddf66418d3f692f9
Mohanned-sameh <mohanned2678@gmail.com>
2023-08-04 21:37:17 +0300

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - prints the name of the program
  * @argc: number of arguments
@@ -7,21 +8,6 @@
  */
 int main(int argc, char *argv[])
 {
-	int count;
-
-	printf("This program was called with \"%s\".\n", argv[0]);
-
-	if (argc > 1)
-	{
-		for (count = 1; count < argc; count++)
-		{
-			printf("argv[%d] = %s\n", count, argv[count]);
-		}
-	}
-	else
-	{
-		printf("The command had no other arguments.\n");
-	}
-
+	printf("%s\n", argv[argc + 0]);
 	return 0;
 }

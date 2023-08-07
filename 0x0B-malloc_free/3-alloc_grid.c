6011671f0350eec8324390c2937e2c0d818f8399
Mohanned-sameh <mohanned2678@gmail.com>
2023-08-07 08:41:05 +0300

    fixed betty format

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,28 +11,29 @@
 
 int **alloc_grid(int width, int height)
 {
-    int **grid;
-    int i, j;
-    if (width < 1 || height < 1)
-        return (NULL);
-    grid = malloc(height * sizeof(int *));
-    if (grid == NULL)
-    {
-        free(grid);
-        return (NULL);
-    }
-    for (i = 0; i < height; i++)
-    {
-        grid[i] = malloc(width * sizeof(int));
-        if (grid[i] == NULL)
-        {
-            for (i--; i >= 0; i--)
-                free(grid[i]);
-            free(grid);
-            return (NULL);
-        }
-        for (j = 0; j < width; j++)
-            grid[i][j] = 0;
-    }
-    return (grid);
+	int **grid;
+	int i, j;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		free(grid);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
 }

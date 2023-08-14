8fdff55ec3d39d5b71e3aa51678fa7512a94bc3b
Mohanned-sameh <mohanned2678@gmail.com>
2023-08-14 16:03:38 +0300

    fixed dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure for dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+#endif

d1302843921160ae743d8d9a7854ad45b3862d93
Mohanned-sameh <mohanned2678@gmail.com>
2023-08-16 22:26:49 +0300

    fixed betty

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,26 +5,26 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*p)(int, int);
+	int num1, num2, result;
+	int (*p)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    p = get_op_func(argv[2]);
-    if (p == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
-    result = p(num1, num2);
+	p = get_op_func(argv[2]);
+	if (p == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = p(num1, num2);
 
-    printf("%d\n", result);
-    return (0);
+	printf("%d\n", result);
+	return (0);
 }

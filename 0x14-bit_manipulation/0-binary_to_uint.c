81ca816577912060b85d131fc693e8dbc2326d5f
Mohanned <Mohanned2678@gmail.com>
2023-09-02 00:07:48 +0300

    fixed requirments

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
- * binary_to_unit - converts binary to unit
- * @b: binary
- * Return: unit
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
  */
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int unit = 0;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
@@ -16,7 +18,8 @@ unsigned int binary_to_unit(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		unit = 2 * unit + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (unit);
+
+	return (dec_val);
 }

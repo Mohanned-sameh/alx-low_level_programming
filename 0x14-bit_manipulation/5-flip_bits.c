b2f673be598682e770030918d893458cbb89b063
Mohanned <Mohanned2678@gmail.com>
2023-09-02 00:24:17 +0300

    fixed betty requirments

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,11 +9,11 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned int nbits;
-    for (nbits = 0; n || m; n >>= 1, m >>= 1)
-    {
-        if ((n & 1) != (m & 1))
-            nbits++;
-    }
-    return (nbits);
+	unsigned int nbits;
+	for (nbits = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			nbits++;
+	}
+	return (nbits);
 }

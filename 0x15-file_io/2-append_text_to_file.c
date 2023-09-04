077bd78a19bbafd7d6fc6ef88a17a395a7f75a03
Mohanned <Mohanned2678@gmail.com>
2023-09-04 14:27:45 +0300

    fixed errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,20 +7,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, t, s = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
-		for (s = 0; text_content[s]; s++)
-			s++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	fd = open(filename, O_WRONLY | O_APPEND);
-	t = write(fd, text_content, s);
 
-	if (fd == -1 || t == -1)
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
-	close(fd);
+
+	close(o);
+
 	return (1);
 }

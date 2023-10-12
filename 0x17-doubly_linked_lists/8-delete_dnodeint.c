e0789931392a227deb096c155823ccae1a05e1cc
Mohanned-sameh <mohanned2678@gmail.com>
2023-10-13 02:44:00 +0300

    done

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,32 +1,40 @@
 #include "lists.h"
 /**
- * delete_nodeint_at_index - deletes the node at index
+ * delete_dnodeint_at_index - deletes the node at index
  * @head: pointer to the head of the list
  * @index: index of the node to be deleted
  * Return: 1 if it succeeded, -1 if it failed
  */
-int delete_nodeint_at_index(dlistint_t **head, unsigned int index)
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp;
-	dlistint_t *current;
-	unsigned int i;
-	if (*head == NULL)
+	dlistint_t *node = *head;
+	dlistint_t *node_to_delete = *head;
+	unsigned int i, count = 0;
+
+	if (!(*head))
 		return (-1);
-	current = *head;
 	if (index == 0)
 	{
-		*head = current->next;
-		free(current);
+		*head = node_to_delete->next;
+		free(node_to_delete);
+		if (*head)
+			(*head)->prev = NULL;
 		return (1);
 	}
-	for (i = 0; i < index - 1; i++)
+	i = index - 1;
+	while (node && count != i)
 	{
-		if (current->next == NULL)
-			return (-1);
-		current = current->next;
-		temp = current->next;
-		current->next = temp->next;
+		count++;
+		node = node->next;
 	}
-	free(temp);
-	return (1);
+	if (count == i && node)
+	{
+		node_to_delete = node->next;
+		if (node_to_delete->next)
+			node_to_delete->next->prev = node;
+		node->next = node_to_delete->next;
+		free(node_to_delete);
+		return (1);
+	}
+	return (-1);
 }

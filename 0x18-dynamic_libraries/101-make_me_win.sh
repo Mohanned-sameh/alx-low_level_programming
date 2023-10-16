6ebcc0978e2c23c73c87c27bd69b30928debaa42
Mohanned-sameh <mohanned2678@gmail.com>
2023-10-16 20:05:58 +0300

    fixed link

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
+wget -O /tmp/win.so https://github.com/alx-tools/0x18.c
 export LD_PRELOAD=/tmp/win.so

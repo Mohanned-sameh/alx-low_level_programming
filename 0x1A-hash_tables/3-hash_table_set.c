bb23c43d707c3912de9f10785f619ccb5f10eb42
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-02 22:12:35 +0200

    fixed all leaks possible

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,47 +4,61 @@
  * @ht: hash table
  * @key: key
  * @value: value
- * Return: 1 if it succeeded, 0 otherwise
+ * Return: 1 if it succeeded, (0) otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new_node, *current;
 
-	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
+	if (key == NULL || ht == NULL || strcmp(key, "") == 0)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
 	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 	new_node->next = NULL;
 	if (ht->array[index] == NULL)
+	{
 		ht->array[index] = new_node;
+		return (1);
+	}
 	else
 	{
 		current = ht->array[index];
-		if (strcmp(current->key, key) == 0)
+
+		while (current != NULL)
 		{
-			new_node->next = current->next;
-			ht->array[index] = new_node;
-			free(current);
-			return (1);
-		}
-		while (current->next != NULL && strcmp(current->next->key, key))
+			if (strcmp(current->key, key) == 0)
+			{
+				free(new_node->key);
+				free(new_node->value);
+				free(new_node);
+				free(current->value); // Free the old value
+				current->value = strdup(value);
+				if (current->value == NULL)
+					return (0);
+				return (1);
+			}
 			current = current->next;
-		if (strcmp(current->key, key) == 0)
-		{
-			new_node->next = current->next;
-			free(current->next);
-			current->next = new_node;
 		}
-		else
-		{
-			new_node->next = ht->array[index];
-			ht->array[index] = new_node;
-		}
+		current = ht->array[index];
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new_node;
+		return (1);
 	}
-	return (1);
 }

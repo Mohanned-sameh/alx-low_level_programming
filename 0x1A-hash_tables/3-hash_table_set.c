8178b2b25c3e12698766813cdd8e2c951df40d5c
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-02 21:39:05 +0200

    fixed betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,43 +8,43 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *new_node, *current;
+	unsigned long int index;
+	hash_node_t *new_node, *current;
 
-    if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
-        return (0);
-    index = key_index((unsigned char *)key, ht->size);
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-        return (0);
-    new_node->key = strdup(key);
-    new_node->value = strdup(value);
-    new_node->next = NULL;
-    if (ht->array[index] == NULL)
-        ht->array[index] = new_node;
-    else
-    {
-        current = ht->array[index];
-        if (strcmp(current->key, key) == 0)
-        {
-            new_node->next = current->next;
-            ht->array[index] = new_node;
-            free(current);
-            return (1);
-        }
-        while (current->next != NULL && strcmp(current->next->key, key))
-            current = current->next;
-        if (strcmp(current->key, key) == 0)
-        {
-            new_node->next = current->next;
-            free(current->next);
-            current->next = new_node;
-        }
-        else
-        {
-            new_node->next = ht->array[index];
-            ht->array[index] = new_node;
-        }
-    }
-    return (1);
+	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+	if (ht->array[index] == NULL)
+		ht->array[index] = new_node;
+	else
+	{
+		current = ht->array[index];
+		if (strcmp(current->key, key) == 0)
+		{
+			new_node->next = current->next;
+			ht->array[index] = new_node;
+			free(current);
+			return (1);
+		}
+		while (current->next != NULL && strcmp(current->next->key, key))
+			current = current->next;
+		if (strcmp(current->key, key) == 0)
+		{
+			new_node->next = current->next;
+			free(current->next);
+			current->next = new_node;
+		}
+		else
+		{
+			new_node->next = ht->array[index];
+			ht->array[index] = new_node;
+		}
+	}
+	return (1);
 }
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,22 +9,20 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *current;
+	unsigned long int index;
+	hash_node_t *current;
 
-    if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
-        return (NULL);
+	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
+		return (NULL);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    current = ht->array[index];
-    if (current == NULL)
-        return (NULL);
-    while (strcmp(current->key, key) && current != NULL)
-    {
-        current = current->next;
-    }
-    if (current == NULL)
-        return (NULL);
-    else
-        return (current->value);
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	if (current == NULL)
+		return (NULL);
+	while (strcmp(current->key, key) && current != NULL)
+		current = current->next;
+	if (current == NULL)
+		return (NULL);
+	else
+		return (current->value);
 }

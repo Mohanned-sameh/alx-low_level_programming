f3140746a32990fc2ba78c708145cd40d050940f
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-03 12:37:02 +0200

    idk

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,57 +8,42 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node, *current;
-
-	if (key == NULL || ht == NULL || strcmp(key, "") == 0)
+	if (!key || !ht || !*key)
 		return (0);
-	index = key_index((unsigned char *)key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	unsigned long int index = key_index((unsigned char *)key, ht->size);
+	hash_node_t *current = ht->array[index];
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
+	if (!new_node)
 		return (0);
 	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
 	new_node->value = strdup(value);
-	if (new_node->value == NULL)
+	if (!new_node->key || !new_node->value)
 	{
 		free(new_node->key);
+		free(new_node->value);
 		free(new_node);
 		return (0);
 	}
 	new_node->next = NULL;
-	if (ht->array[index] == NULL)
-	{
+	if (!current)
 		ht->array[index] = new_node;
-		return (1);
-	}
 	else
 	{
-		current = ht->array[index];
-
-		while (current != NULL)
+		while (current->next && strcmp(current->key, key) != 0)
+			current = current->next;
+		if (strcmp(current->key, key) == 0)
 		{
-			if (strcmp(current->key, key) == 0)
-			{
-				free(new_node->key);
-				free(new_node->value);
-				free(new_node);
-				free(current->value); // Free the old value
-				current->value = strdup(value);
-				if (current->value == NULL)
-					return (0);
-				return (1);
-			}
-			current = current->next;
+			free(current->value);
+			current->value = strdup(value);
+			free(new_node->key);
+			free(new_node->value);
+			free(new_node);
+			if (!current->value)
+				return (0);
 		}
-		current = ht->array[index];
-		while (current->next != NULL)
-			current = current->next;
-		current->next = new_node;
-		return (1);
+		else
+			current->next = new_node;
 	}
+	return (1);
 }

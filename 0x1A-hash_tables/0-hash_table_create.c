6f4a21d9e5b054b1c98e3ca7eaa739f1fa172e11
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-03 12:39:22 +0200

    done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,9 +6,10 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	hash_table_t *ht;
 	unsigned long int i;
 
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 	ht->array = malloc(sizeof(hash_node_t *) * size);

3c4a2f5ddb5671858094ac182fb4c6c34ccc0a61
Mohanned-sameh <mohanned2678@gmail.com>
2023-11-03 13:23:35 +0200

    im done

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,9 +9,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int result = (hash_djb2(key) % size);
 
-	if (size == 0)
-		if (key == NULL || size == 0)
-			return (0);
-
+	if (key == NULL || size == 0)
+		return (0);
 	return (result);
 }

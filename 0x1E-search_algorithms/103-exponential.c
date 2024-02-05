14c1a26665e9bf39673e3f1f42f7513ed949374c
mohanned-sameh <mohanned2678@gmail.com>
2024-02-05 18:13:05 +0200

    fixed exponential algorithms

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,5 +1,29 @@
 #include "search_algos.h"
 /**
+ * _binary_search - searches for a value in a sorted array of integers
+ * using the Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @left: left bound of the array
+ * @right: right bound of the array
+ * @value: value to search for
+ * Return: first index where value is located or -1 if value is not present
+ */
+int _binary_search(int *array, size_t left, size_t right, int value)
+{
+	size_t mid;
+
+	if (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] > value)
+			return (_binary_search(array, left, mid - 1, value));
+		return (_binary_search(array, mid + 1, right, value));
+	}
+	return (-1);
+}
+/**
  * exponential_search - searches for a value in a sorted array of integers
  * using the Exponential search algorithm
  * @array: pointer to the first element of the array to search in
@@ -9,16 +33,16 @@
  */
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t i = 1, min;
+	size_t bound = 1, min;
 
-	if (!array)
+	if (array == NULL || size == 0)
 		return (-1);
-	while (i < size && array[i] <= value)
+	while (bound < size && array[bound] < value)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		i *= 2;
+		printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+		bound *= 2;
 	}
-	min = i < size - 1 ? i : size - 1;
-	printf("Value found between indexes [%lu] and [%lu]\n", i / 2, min);
-	return (binary_search(array, min + 1, value));
+	min = (bound < size - 1) ? bound : size - 1;
+	printf("Value found between indexes [%lu] and [%lu]\n", bound / 2, min);
+	return (_binary_search(array, bound / 2, min, value));
 }
